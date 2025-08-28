ee68b0eea00b960c6df0cf71cf8da5c189b47c97
Rahul Arora <rahularora2715@gmail.com>
2025-08-29 01:04:14 +0530

    removed the blog posts page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import Blog from "./components/Blog";
 import Projects from "./components/Projects";
 import ProjectDetail from "./components/ProjectDetail";
 
@@ -11,7 +10,6 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/blogs" element={<Blog />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/projects/:projectId" element={<ProjectDetail />} />
       </Routes>
